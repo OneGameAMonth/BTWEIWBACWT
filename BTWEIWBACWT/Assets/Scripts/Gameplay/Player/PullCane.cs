9c9278c273080e3d0d89e5e07c19d659a8c7242b
Vladimir de la Cruz <vladimirdlc@gmail.com>
2013-05-26 09:25:29 -0430

    Basic Rod Physics

---
+++ BTWEIWBACWT/Assets/Scripts/Gameplay/Player/PullCane.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System.Collections;
+
+public class PullCane : MonoBehaviour {
+    public float caneFallingTime = 0.1f;
+    public float maxScale = 1.0f;
+    public float minScale = 0.1f;
+    public float smoothDamp = 100.0f;
+    private float currentTime;
+    public float augmentationStep = 0.02f;
+    public float disminutionStep = 0.01f;
+    public Vector3 scaleVector = new Vector3(0, 1);
+    public Vector3 targetScale;
+
+    void Start()
+    {
+        targetScale = transform.localScale;
+    }
+
+    void FixedUpdate()
+    {
+        currentTime += Time.fixedDeltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (this.transform.localScale.y >= minScale)
+            {
+                targetScale = new Vector3(
+                    targetScale.x - disminutionStep * scaleVector.x,
+                    targetScale.y - disminutionStep * scaleVector.y,
+                    targetScale.z - disminutionStep * scaleVector.z);
+            }
+        }
+        else if (currentTime >= caneFallingTime)
+        {
+            if (this.transform.localScale.y < maxScale)
+            {
+                targetScale = new Vector3(
+                                    targetScale.x + augmentationStep * scaleVector.x,
+                                    targetScale.y + augmentationStep * scaleVector.y,
+                                    targetScale.z + augmentationStep * scaleVector.z);
+            }
+
+            currentTime = 0;
+        };
+
+        transform.localScale = Vector3.Lerp(transform.localScale, targetScale, Time.fixedDeltaTime * smoothDamp);
+    }
+}
