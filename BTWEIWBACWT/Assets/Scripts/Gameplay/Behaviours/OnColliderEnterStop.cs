46738650c9996d87b6a66afc9b059b62f3c63720
Vladimir de la Cruz <vladimirdlc@gmail.com>
2013-05-30 19:18:29 -0430

    Corrected walls

--- BTWEIWBACWT/Assets/Scripts/Gameplay/Behaviours/OnColliderEnterStop.cs
+++ BTWEIWBACWT/Assets/Scripts/Gameplay/Behaviours/OnColliderEnterStop.cs
@@ -2,14 +2,8 @@ using UnityEngine;
 using System.Collections;
 
 public class OnColliderEnterStop : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.rigidbody) collision.gameObject.rigidbody.velocity = Vector2.zero;
+    }
 }
